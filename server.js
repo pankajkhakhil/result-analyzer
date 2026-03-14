ecbb73501529dddbe3a00c2f5cf960aba6294583
pankajkhakhil <pankajkhakhil@gmail.com>
2026-03-15 01:44:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const axios = require("axios");
 const cheerio = require("cheerio");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
