c241b421773718a2aa602c370227838e6143ff7e
pankajkhakhil <pankajkhakhil@gmail.com>
2026-03-15 01:31:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -169,4 +169,8 @@ app.post("/analyze", async (req, res) => {
     res.json({ students, summary, top3 });
 });
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
